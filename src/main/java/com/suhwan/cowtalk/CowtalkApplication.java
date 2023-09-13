ae8b52a49c8a6a2eb1eaa258526636a2454c8e75
9898s <sueh9898@icloud.com>
2023-09-13 11:45:00 +0900

    initial commit

---
+++ src/main/java/com/suhwan/cowtalk/CowtalkApplication.java
@@ -0,0 +1,13 @@
+package com.suhwan.cowtalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CowtalkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CowtalkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/suhwan/cowtalk/CowtalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suhwan.cowtalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CowtalkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
