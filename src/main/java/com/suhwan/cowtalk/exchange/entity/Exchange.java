209ead82b226c884dd39709cac979ead8ee4ba03
9898s <sueh9898@icloud.com>
2023-09-16 13:45:42 +0900

    feat: add exchange entity

---
+++ src/main/java/com/suhwan/cowtalk/exchange/entity/Exchange.java
@@ -0,0 +1,33 @@
+package com.suhwan.cowtalk.exchange.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Entity
+public class Exchange {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private LocalDateTime createDate;
+
+    @Column
+    private LocalDateTime updateDate;
+
+    @Column
+    private LocalDateTime deleteDate;
+}
