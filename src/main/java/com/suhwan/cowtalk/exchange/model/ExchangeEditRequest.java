24e6f874b3c73dd51aaa6aca429a6714251c4bde
9898s <sueh9898@icloud.com>
2023-09-16 18:08:18 +0900

    test: add exchange update test

--- src/main/java/com/suhwan/cowtalk/exchange/model/ExchangeEditRequest.java
+++ src/main/java/com/suhwan/cowtalk/exchange/model/ExchangeEditRequest.java
@@ -1,7 +1,11 @@
 package com.suhwan.cowtalk.exchange.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
+@NoArgsConstructor
 @Getter
 public class ExchangeEditRequest {
 
--- src/test/java/com/suhwan/cowtalk/exchange/controller/ExchangeApiControllerTest.java
+++ src/test/java/com/suhwan/cowtalk/exchange/controller/ExchangeApiControllerTest.java
@@ -2,6 +2,8 @@ package com.suhwan.cowtalk.exchange.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.suhwan.cowtalk.exchange.entity.Exchange;
+import com.suhwan.cowtalk.exchange.model.ExchangeEditRequest;
+import com.suhwan.cowtalk.exchange.model.ExchangeEditResponse;
 import com.suhwan.cowtalk.exchange.service.ExchangeService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,11 +12,10 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -64,5 +65,33 @@ class ExchangeApiControllerTest {
                 .andExpect(jsonPath("$.id").value("1"))
                 .andExpect(jsonPath("$.name").value("코인빗"))
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    void 거래소를_수정한다() throws Exception {
+        // given
+        given(exchangeService.updateExchange(any(ExchangeEditRequest.class)))
+                .willReturn(Exchange.builder()
+                        .id(1L)
+                        .name("코인빗")
+                        .build());
+
+        // when
+        when(exchangeService.updateExchange(any(ExchangeEditRequest.class)))
+                .thenReturn(Exchange.builder()
+                        .id(1L)
+                        .name("코빗")
+                        .build());
+
+        // then
+        mockMvc.perform(put("/api/exchange/edit")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(
+                                new ExchangeEditRequest(1L, "코빗")
+                        )))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value("1"))
+                .andExpect(jsonPath("$.name").value("코빗"))
+                .andDo(print());
     }
 }
