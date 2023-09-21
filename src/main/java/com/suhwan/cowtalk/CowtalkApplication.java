5580fee17ca9fd0b71a655e23738335383d721e1
9898s <sueh9898@icloud.com>
2023-09-21 18:29:52 +0900

    feat: batch, scheduler 활성화

--- src/main/java/com/suhwan/cowtalk/CowtalkApplication.java
+++ src/main/java/com/suhwan/cowtalk/CowtalkApplication.java
@@ -1,13 +1,17 @@
 package com.suhwan.cowtalk;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
+@EnableBatchProcessing
 @SpringBootApplication
 public class CowtalkApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(CowtalkApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(CowtalkApplication.class, args);
+  }
 
 }
