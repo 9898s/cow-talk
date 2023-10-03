2b07518b5c40b672482cb38daf8835032e341a36
9898s <sueh9898@icloud.com>
2023-10-03 11:40:27 +0900

    feat: 댓글 좋아요/싫어요 캐싱 추가

---
+++ src/main/java/com/suhwan/cowtalk/comment/repository/CommentGoodBadCacheRepository.java
@@ -0,0 +1,8 @@
+package com.suhwan.cowtalk.comment.repository;
+
+import com.suhwan.cowtalk.comment.model.goodbad.CommentGoodBadCache;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CommentGoodBadCacheRepository extends CrudRepository<CommentGoodBadCache, String> {
+
+}
